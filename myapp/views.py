ec2da6c1bc06c1cd93597053c668224447051eb4
hyedinion <ja03129@naver.com>
2021-06-01 02:12:39 +0900

    update

--- myapp/views.py
+++ myapp/views.py
@@ -45,8 +45,8 @@ def kakao_pay(request):
         data = request.session['data']
         params = {
             "cid": "TC0ONETIME",    # 테스트용 코드
-            "partner_order_id": "1001",     # 주문번호
-            "partner_user_id": "mybrew",    # 유저 아이디
+            "partner_order_id": str(data['id']),     # 주문번호
+            "partner_user_id": str(data['name']),    # 유저 아이디
             "item_name": 'mybrew',        # 구매 물품 이름
             "quantity": '1',                # 구매 물품 수량
             "total_amount": '2000',        # 구매 물품 가격
@@ -68,6 +68,7 @@ def kakao_pay(request):
     return redirect('approval')
 
 def approval(request):
+    data = request.session['data']
     URL = 'https://kapi.kakao.com/v1/payment/approve'
     headers = {
         "Authorization": "KakaoAK " + "4cac6618fb4a521801d73c5c0a3ab8a9",
@@ -76,8 +77,8 @@ def approval(request):
     params = {
         "cid": "TC0ONETIME",    # 테스트용 코드
         "tid": request.session['tid'],  # 결제 요청시 세션에 저장한 tid
-        "partner_order_id": "1001",     # 주문번호
-        "partner_user_id": "mybrew",    # 유저 아이디
+        "partner_order_id": str(data['id']),     # 주문번호
+        "partner_user_id": str(data['name']),    # 유저 아이디
         "pg_token": request.GET.get("pg_token"),     # 쿼리 스트링으로 받은 pg토큰
     }
 
